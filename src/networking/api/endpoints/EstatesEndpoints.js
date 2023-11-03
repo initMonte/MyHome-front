0733122955ae7878eeb7f3f4fedbd13fe37c6e18
Matias Monteverde <maty9696@gmail.com>
2023-11-03 18:06:15 -0300

    funciona crear propiedad!

--- src/networking/api/endpoints/EstatesEndpoints.js
+++ src/networking/api/endpoints/EstatesEndpoints.js
@@ -34,46 +34,52 @@ export let estatesWS = {
     expenses,
     latitude,
     longitude,
-    files,
+    file,
     videoUrl,
     realEstateId,
   ) {
     console.log(typeof expenses);
-    return await Api.post(urlApi.estate.createEstate, {
-      title,
-      description,
-      rentOrSale,
-      status,
-      street,
-      addressNumber,
-      floor,
-      neighborhood,
-      state,
-      country,
-      estateType,
-      coveredSquareMeters,
-      semiUncoveredSquaremeters,
-      uncoveredSquareMeters,
-      roomsAmount,
-      bathroomsAmount,
-      bedroomsAmount,
-      terrace,
-      balcony,
-      storage,
-      garage,
-      frontOrBack,
-      antiquity,
-      orientation,
-      amenites,
-      currency,
-      price,
-      expenseCurrency,
-      expenses,
-      latitude,
-      longitude,
-      files,
-      videoUrl,
-      realEstateId,
+    const formData = new FormData(); // Create a FormData object
+
+    // Append all the variables to the FormData object
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('rentOrSale', rentOrSale);
+    formData.append('status', status);
+    formData.append('street', street);
+    formData.append('addressNumber', addressNumber);
+    formData.append('floor', floor);
+    formData.append('neighborhood', neighborhood);
+    formData.append('state', state);
+    formData.append('country', country);
+    formData.append('estateType', estateType);
+    formData.append('coveredSquareMeters', coveredSquareMeters);
+    formData.append('semiUncoveredSquaremeters', semiUncoveredSquaremeters);
+    formData.append('uncoveredSquareMeters', uncoveredSquareMeters);
+    formData.append('roomsAmount', roomsAmount);
+    formData.append('bathroomsAmount', bathroomsAmount);
+    formData.append('bedroomsAmount', bedroomsAmount);
+    formData.append('terrace', terrace);
+    formData.append('balcony', balcony);
+    formData.append('storage', storage);
+    formData.append('garage', garage);
+    formData.append('frontOrBack', frontOrBack);
+    formData.append('antiquity', antiquity);
+    formData.append('orientation', orientation);
+    formData.append('amenites', amenites);
+    formData.append('currency', currency);
+    formData.append('price', price);
+    formData.append('expenseCurrency', expenseCurrency);
+    formData.append('expenses', expenses);
+    formData.append('latitude', latitude);
+    formData.append('longitude', longitude);
+    formData.append('file', file);
+    formData.append('videoUrl', videoUrl);
+    formData.append('realEstateId', realEstateId);
+    return await Api.post(urlApi.estate.createEstate, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data', // Set the appropriate content type for file uploads
+      },
     });
   },
   getEstatesByUserId: async function (id) {
